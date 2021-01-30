71e047a646439e62b6187925d61e0140d0c61bad
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-30 01:10:55 -0500

    some startup stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Client } from 'discord.js';
 import { readFile } from 'fs/promises';
 import { existsSync } from 'fs';
+import { join } from 'path';
 import startup from './util/startup';
 import db from './util/db';
 import Shop from './rpg/shop';
-import { join } from 'path';
 import { readFullDir } from './util/utils';
 
 const NODE_ENV = (process.env.NODE_ENV ||= process.argv.includes('-d')
@@ -18,20 +18,25 @@ const NODE_ENV = (process.env.NODE_ENV ||= process.argv.includes('-d')
 		const ascii = (await readFile('ascii.txt')).toString();
 		console.log(ascii);
 	}
+})().then(async function start() {
+	try {
+		// Extensions
+		const extensionFiles = await readFullDir(join(__dirname, 'extensions'));
+		await Promise.all(extensionFiles.map(file => import(file)));
 
-	// Extensions
-	const extensionFiles = await readFullDir(join(__dirname, 'extensions'));
-	await Promise.all(extensionFiles.map(file => import(file)));
+		console.log(`Running in ${NODE_ENV} mode`);
 
-	console.log(`Running in ${NODE_ENV} mode`);
+		await db();
+		console.log('MongoDB connected!');
 
-	await db();
-	console.log('MongoDB connected!');
+		const client = new Client({ restTimeOffset: 300 });
+		client.shop = await Shop.init();
+		await startup(client);
 
-	const client = new Client({ restTimeOffset: 300 });
-	client.shop = await Shop.init();
-	await startup(client);
-
-	console.log('Logging in...');
-	client.login(process.env.TOKEN);
-})().catch(console.error);
+		console.log('Logging in...');
+		client.login(process.env.TOKEN);
+	} catch (err) {
+		console.error('Startup error:', err);
+		setTimeout(start, 2000);
+	}
+});
